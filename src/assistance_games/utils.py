c6818176ad6ca5439d26ee917096163f8f203b5d
Dmitrii <dmkr0001@gmail.com>
2022-07-08 15:50:28 +0100

    Cosmetic changes

--- src/assistance_games/utils.py
+++ src/assistance_games/utils.py
@@ -5,12 +5,15 @@ import sparse
 
 _ASSETS = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets')
 
+
 def get_asset(name):
     return os.path.join(_ASSETS, name)
+
 
 def sample_distribution(p):
     p = force_dense(p)
     return np.random.choice(np.arange(len(p)), p=p)
+
 
 def uniform_simplex_sample(n):
     """Uniform sample of n-dim point in 0 <= x_i <= 1, sum_i x_i = 1.
@@ -21,6 +24,7 @@ def uniform_simplex_sample(n):
     diffs = arr[1:] - arr[:-1]
     vals = diffs[np.random.permutation(n)]
     return vals
+
 
 def dict_to_sparse(d, shape):
     points = []
@@ -33,11 +37,13 @@ def dict_to_sparse(d, shape):
 
     return sparse.COO(coords, values, shape=shape)
 
+
 def force_sparse(arr):
     if isinstance(arr, sparse.COO):
         return arr
     else:
         return sparse.COO(arr)
+
 
 def force_dense(arr):
     if isinstance(arr, sparse.COO):
